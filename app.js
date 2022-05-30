f1ea3b5a9ea2703039e8c6ff963d1e3a09e4d750
Emily Phee <ephee@uci.edu>
2022-05-29 21:27:43 -0700

    Create operate function and make calculator calculate with entered number & operator

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+const btnKeys = document.querySelectorAll('.btn-key');
+const displayText = document.querySelector('.display-text');
+
+let displayValue = '';
+let currentOperator;
+let solution;
+
+let firstNum;
+let secondNum;
+
+btnKeys.forEach(key => {
+  key.addEventListener('click', () => {
+    const clickedkeyVal = key.innerText;
+
+    // check if pressed key is number
+    if (/^\d+$/.test(clickedkeyVal) || clickedkeyVal === '.') {
+      displayText.classList.add('active');
+      displayValue += clickedkeyVal;
+      if (displayValue.length < 110) {
+        displayText.innerText = displayValue;
+      }
+    }
+
+    // check if pressed key is operator
+    if (/(\+|-|\*|\/)$/.test(clickedkeyVal)) {
+      firstNum = displayValue;
+      currentOperator = clickedkeyVal;
+      displayValue = '';
+    }
+
+    // check if pressed key is equal sign (enter)
+    if (/=$/.test(clickedkeyVal)) {
+      secondNum = displayValue;
+
+      solution = operate(
+        currentOperator,
+        parseInt(firstNum),
+        parseInt(secondNum)
+      );
+
+      displayText.innerText = solution;
+    }
+  });
+});
+
 // math operators
 function add(a, b) {
   return a + b;
