41f7ed1df6a9476573a4cc09504927f7747a55cb
Emily Phee <ephee@uci.edu>
2022-05-30 15:02:46 -0700

    Fix CSS Calculate display

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const displayText = document.querySelector('.display-text');
 
 let displayValue = '';
 let currentOperator;
+let prevOperator;
+
 let solution;
 
 let firstNum;
 let secondNum;
+
+let calculateCounter = 0;
 
 btnKeys.forEach(key => {
   key.addEventListener('click', () => {
@@ -23,20 +27,33 @@ btnKeys.forEach(key => {
 
     // check if pressed key is operator
     if (/(\+|-|\*|\/)$/.test(clickedkeyVal)) {
-      firstNum = displayValue;
-      currentOperator = clickedkeyVal;
-      displayValue = '';
+      calculateCounter++;
+      prevOperator = clickedkeyVal;
+
+      if (calculateCounter > 1) {
+        currentOperator = clickedkeyVal;
+        secondNum = displayValue;
+        console.log('first', firstNum);
+        console.log('second', secondNum);
+        firstNum = operate(prevOperator, firstNum, secondNum);
+
+        console.log('first num', firstNum);
+        calculateCounter = 0;
+        displayValue = '';
+      } else {
+        firstNum = displayValue;
+        currentOperator = clickedkeyVal;
+        displayValue = '';
+
+        console.log(calculateCounter);
+      }
     }
 
     // check if pressed key is equal sign (enter)
     if (/=$/.test(clickedkeyVal)) {
       secondNum = displayValue;
 
-      solution = operate(
-        currentOperator,
-        parseInt(firstNum),
-        parseInt(secondNum)
-      );
+      solution = operate(currentOperator, firstNum, secondNum);
 
       displayText.innerText = solution;
     }
@@ -61,14 +78,18 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  if (operator === '+') {
-    return add(a, b);
-  } else if (operator === '-') {
-    return subtract(a, b);
-  } else if (operator === '*') {
-    return multiply(a, b);
-  } else if (operator === '/') {
-    return divide(a, b);
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+
+    case '/':
+      return divide(a, b);
   }
 }
 
